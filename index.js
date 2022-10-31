c266b2dbfb2a7e2f6ad388ef7b3fce7e3c2b8f23
Mithilesh-create <mithileshsharma539@gmail.com>
2022-10-31 21:46:52 +0530

    make it better

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const url = process.env.DB_CONNECT;
 const client = new MongoClient(url);
 
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 8000;
 // Database Name
-const dbName = process.env.DBNAME;
+const dbName = process.env.DB_NAME;
 
 async function main() {
   await client.connect();
@@ -25,6 +26,4 @@ async function main() {
   return "done.";
 }
 
-main()
-  .then(console.log)
-  .catch(console.error);
+main().then(console.log).catch(console.error);
